e07b2de87d27f625f107bd22f37bd956a0d4909b
Azam-aa <azamp442@gmail.com>
2025-08-21 14:24:24 +0530

    dice two is ready

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ var randomImageSource = "images/"+randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src",randomImageSource)
+image1.setAttribute("src",randomImageSource);
+
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
