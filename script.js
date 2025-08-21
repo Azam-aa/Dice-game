97c6a984c968c02b59530a5e10498006306dea6b
Azam-aa <azamp442@gmail.com>
2025-08-21 14:27:38 +0530

    Added conditions

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ let randomNumber2 = Math.floor(Math.random()*6)+1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+if (randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 won";
+}
+else if( randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 won";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
